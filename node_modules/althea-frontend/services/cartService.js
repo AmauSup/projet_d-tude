d99964b0a7eee91204b8ac711d3df2f5b369a89d
AmauSup <amaury.demaille@supdevinci-edu.fr>
2026-04-25 15:27:33 +0200

    connection bdd

--- node_modules/althea-frontend/services/cartService.js
+++
@@ -1,9 +0,0 @@
-import { buildCartDetails, computeCartSummary } from '../utils/storefront.js';
-
-export const cartService = {
-  build(cartItems, products) {
-    const details = buildCartDetails(cartItems, products);
-    const summary = computeCartSummary(details);
-    return { details, summary };
-  },
-};
--- node_modules/althea-frontend/services/categoryService.js
+++
@@ -1,9 +0,0 @@
-const wait = (ms = 100) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export const categoryService = {
-  async list(categories) {
-    await wait();
-    // Backend hook: GET /categories
-    return categories;
-  },
-};
